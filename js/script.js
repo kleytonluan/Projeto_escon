981f856d000e71bbfc718fbf850f71c86b25186b
maquina06 <maquina06@bukyramos@gmail.com>
2019-06-08 14:48:20 -0300

    varias alteração, cadastro militar add

--- js/script.js
+++ js/script.js
@@ -26,3 +26,119 @@ $(document).ready(function () {
 				a.click();
 			});
 		});
+
+var idContador=0;
+function exclui(id){
+	var campo = $("#"+id.id);
+	campo.hide(200);
+}
+		
+$( document ).ready(function() {
+				
+		$("#btnAdicionaItem").click(function(e){
+			e.preventDefault();
+			var tipoCampo = "item";
+			adicionaCampo(tipoCampo);
+		})
+				
+		$("#btnAdicionaTelefone").click(function(e){
+			e.preventDefault();
+			var tipoCampo = "telefone";
+			adicionaCampo(tipoCampo);
+		})	
+				
+			function adicionaCampo(tipo){
+		
+				idContador++;
+					
+				var idCampo = "campoExtra"+idContador;
+				var idForm = "formExtra"+idContador;
+				
+				var html = "";
+				html += "<div id='"+idForm+"' class='box-body'>"
+				html += "<div class='row'>"
+                html += "   <div class='form-group col-md-3'>"
+                html += "      <input type='text' class='form-control' id='name' placeholder='Nome completo'>"
+                html += "    </div>"
+                html += "    <div class='form-group col-md-2'>"
+                html += "      <input type='text' class='form-control' id='name' placeholder='Nome de guerra'>"
+                html += "    </div>"
+                html += "    <div class='form-group col-md-1'>"
+                html += "        <p>"
+                html += "            <select class='form-control diasemana' id='posto-grad'>"
+                html += "              <option value='1º Ten'>1º Ten</option>"
+                html += "              <option value='2º Ten'>2º Ten</option>"
+                html += "              <option value='Asp Of'>Asp Of</option>"
+                html += "              <option value='1º Sgt'>1º Sgt</option>"
+                html += "              <option value='2º Sgt'>2º Sgt</option>"
+                html += "              <option value='3º Sgt'>3º Sgt</option>"
+                html += "              <option value='Cb'>Cb</option>"
+                html += "              <option value='Sd'>Sd</option>"
+                html += "            </select>"
+                html += "          </p>"
+                html += "    </div>"
+                html += "    <div class='form-group col-md-1'>"
+                html += "        <p>"
+                html += "          <select class='form-control diasemana' id='cia'>"
+                html += "            <option value='EM'>EM</option>"
+                html += "            <option value='CCAP'>CCAP</option>"
+                html += "            <option value='CEEM'>CEEM</option>"
+                html += "            <option value='2ª CIA'>2ª CIA</option>"
+				html += "          </select>"
+				html += "        </p>"
+				html += "    </div>"
+				html += "    	<div class='form-group col-md-1'>"
+				html +=	"				<button class='btn''teste' onclick='exclui("+idForm+")' type='button'><span class='fa fa-trash'></span></button>";		
+                html += "  		</div>"
+                html += "  	</div>"
+                html += "</div>"
+			
+				//html += "<table id='"+idForm+"' class='table table-hover'>";
+
+	
+				/** html += "<td>";
+				html += "<span class='input-group-btn'>";
+				html +=	"<button class='btn''teste' onclick='exclui("+idForm+")' type='button'><span class='fa fa-trash'></span></button>";
+				html += "</span></div>";
+				html += "</td>";
+				html += "</tr>";
+				html += "</table>";
+				html += "</div>";*/
+		
+					
+				$("#imendaHTML"+tipo).append(html);
+			}
+				
+			$(".btnExcluir").click(function(){
+				console.log("clicou");
+				$(this).slideUp(200);
+			})
+				
+			$("#btnSalvar").click(function(){
+				
+			var mensagem = "";
+			var novosCampos = [];
+			var camposNulos = false;
+				
+				$('.campoDefault').each(function(){
+					if($(this).val().length < 1){
+						camposNulos = true;
+					}
+				});
+				$('.novoCampo').each(function(){
+					if($(this).is(":visible")){
+						if($(this).val().length < 1){
+							camposNulos = true;
+						}
+						mensagem+= $(this).val()+"\n";
+					}
+				});
+					
+				if(camposNulos == true){
+					alert("Atenção: existem campos sem preenchimento");
+				}
+					
+				novosCampos = [];
+			})
+				
+		});
