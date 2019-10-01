5149d69b8105efc25222b090131b8dcba6264a9d
maquina06 <maquina06@bukyramos@gmail.com>
2019-10-01 11:25:46 -0300

    iniciando PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+var cacheName = 'escon-page';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/vendor/fontawesome-free/css/all.min.css',
+  '/css/sb-admin.css',
+  '/vendor/datatables/dataTables.bootstrap4.css'
+
+];
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
