2ac5ff9a2ea2b6b94a60d2aedd89d38c9968cf00
server <server@bukyramos@gmail.com>
2019-10-06 17:46:15 -0300

    começando modulo offline

---
+++ js/firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/4.4.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.4.0/firebase-messaging.js');
+
+var config = {
+apiKey: "AIzaSyAZsFxnu1KUfNHvXcxxT4k_HoA2Vf_s4xI",
+authDomain: "escon-9ba26.firebaseapp.com",
+databaseURL: "https://escon-9ba26.firebaseio.com",
+projectId: "escon-9ba26",
+storageBucket: "escon-9ba26.appspot.com",
+messagingSenderId: "533904781159"
+};
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+	const title = 'Hello World';
+	const options = {
+		body: payload.data.body
+	};
+	return self.registration.showNotification(title, options);
+});
--- sw.js
+++ sw.js
@@ -5,12 +5,13 @@ var filesToCache = [
   '/index.html',
   '/vendor/fontawesome-free/css/all.min.css',
   '/css/sb-admin.css',
-  '/vendor/datatables/dataTables.bootstrap4.css'
+  '/vendor/datatables/dataTables.bootstrap4.css',
+  '/offline/index.html'
 
 ];
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Install');
-  e.waitUntil(
+  event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
@@ -20,10 +21,24 @@ self.addEventListener('install', function(e) {
 self.addEventListener('activate',  event => {
   event.waitUntil(self.clients.claim());
 });
-self.addEventListener('fetch', event => {
+/**self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request, {ignoreSearch:true}).then(response => {
       return response || fetch(event.request);
     })
-  );
+    .catch(() => {
+      return caches.match('/offline/index.html');
+  ))
+});**/
+// Serve from Cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('/offline/index.html');
+      })
+  )
 });
